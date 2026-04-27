812d5ba47d836252cfa9b35ea8f7cf407edc68f6
jauharivarun <vn.jauhry@gmail.com>
2026-04-28 03:08:18 +0530

    minor changes

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Authorization"],
+    expose_headers=["Content-Type", "Authorization"],
 )
 
 # ---------------------------------------------------------------------------
